50eab3db8b74f56f9b26db1309e6931908142c7e
Seth Lachman <lachmanseth17@gmail.com>
2019-05-06 16:14:11 -0400

    Turned save movie links into navlinks.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -11,7 +11,9 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span key={Math.floor(Math.random() * movie.id)} className="saved-movie">{movie.title}</span>
+          <NavLink to={`/movies/${movie.id}`}>
+            <span key={Math.floor(Math.random() * movie.id)} className="saved-movie">{movie.title}</span>
+          </NavLink>
         ))}
         <div className="home-button">
           <Link to="/">Home</Link>
